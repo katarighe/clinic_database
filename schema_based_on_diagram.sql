09226fa74a3058b77ce84ff8f4d136467d026259
katarighe <m.ighe@hotmail.com>
2023-09-26 13:05:14 +0300

    Add comments on each SQL schema item

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,5 +1,7 @@
+/* Create the clinic database */
 CREATE DATABASE clinic;
 
+/* Create the paitents table */
 CREATE TABLE patients (
   id INT PRIMARY KEY,
   date DATE,
@@ -7,6 +9,7 @@ CREATE TABLE patients (
   date_of_birth DATE
 );
 
+/* Create the medical histories table and their relationsips */
 CREATE TABLE medical_histories (
   id INT PRIMARY KEY, 
   admitted_at TIMESTAMP,
@@ -15,12 +18,14 @@ CREATE TABLE medical_histories (
   FOREIGN KEY (patient_id) REFERENCES patients(id)
 );
 
+/* Create the treatments table and their relationsips */
 CREATE TABLE treatments (
   id INT PRIMARY KEY, 
   type VARCHAR(255),
   name VARCHAR(255)
 );
 
+/* Create the invoices table and their relationsips */
 CREATE TABLE invoices (
   id INT PRIMARY KEY,
   total_amount DECIMAL(10,2),
@@ -30,6 +35,7 @@ CREATE TABLE invoices (
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 
+/* Create the invoice items table and their relationsips */
 CREATE TABLE invoice_items (
   id INT PRIMARY KEY,
   unit_price DECIMAL(10,2),
@@ -41,6 +47,7 @@ CREATE TABLE invoice_items (
   FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
 
+/* Create the treatments histories table and their relationsips */
 CREATE TABLE treatments_histories (
   treatment_id INT,
   medical_history_id INT,
@@ -49,8 +56,10 @@ CREATE TABLE treatments_histories (
   FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id)
 );
 
+/* Update the table to add foreign key relationships on the treatments and medical histories */
 ALTER TABLE treatments_histories ADD CONSTRAINT fk_treatment_id FOREIGN KEY (treatment_id) REFERENCES treatments(id);
 ALTER TABLE treatments_histories ADD CONSTRAINT fk_medical_history_id FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
 
+/* Add foreign key indexes for enhanced data integrity and data query optimization */
 CREATE INDEX idx_treatment_id ON treatments_histories(treatment_id);
 CREATE INDEX idx_medical_history_id ON treatments_histories(medical_history_id);
