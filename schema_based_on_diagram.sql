87df9df0f9deb2d9f69df10e74ef416424cc58f9
katarighe <m.ighe@hotmail.com>
2023-09-26 15:41:47 +0300

    Update

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -59,6 +59,8 @@ CREATE TABLE treatments_histories (
 /* Update the table to add foreign key relationships on the treatments and medical histories as well as constraints */
 ALTER TABLE treatments_histories ADD CONSTRAINT fk_treatment_id FOREIGN KEY (treatment_id) REFERENCES treatments(id);
 ALTER TABLE treatments_histories ADD CONSTRAINT fk_medical_history_id FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
+ALTER TABLE medical_histories ADD CONSTRAINT fk_patient FOREIGN KEY(patient_id) REFERENCES patients(id);
+ALTER TABLE invoices_items ADD CONSTRAINT fk_invoice FOREIGN KEY(invoice_id) REFERENCES invoices(id);
 
 /* Add foreign key indexes for enhanced data integrity and data query optimization */
 CREATE INDEX patient_id ON medical_histories (patient_id);
